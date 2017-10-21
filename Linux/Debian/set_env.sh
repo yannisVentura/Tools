1414357aa4b15648d7b2e5389757d0dfc1aa79b0
LadonKnight <v.yannis972@gmail.com>
2017-10-21 19:58:16 +0200

    Update set_env.sh

--- Linux/Debian/set_env.sh
+++ Linux/Debian/set_env.sh
@@ -6,7 +6,6 @@ sudo apt install -y svn;
 sudo apt install -y openssh;
 sudo apt install -y openvpn;
 sudo apt install -y htop;
-sudo apt install -y dropbox;
 sudo add-apt-repository -y "deb https://packages.microsoft.com/repos/vscode stable main" && sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys EB3E94ADBE1229CF && sudo apt-get update && sudo apt-get install code;
 sudo apt install -y geany && sudo apt install -y geany-plugin*;
 sudo apt install -y steam;
@@ -15,7 +14,6 @@ mkdir /home/$USER/Documents/projects/git;
 mkdir /home/$USER/Documents/projects/svn;
 mkdir /home/$USER/Documents/projects/GoogleDrive;
 mkdir /home/$USER/Documents/projects/Master;
-<<<<<<< HEAD
 echo "Create some aliases..."
 echo "alias clone='git clone'" >> /home/$USER/.bash_aliases;
 echo "alias upgrade='sudo apt-get update && upgrade'" >> /home/$USER/.bash_aliases;
